87b74946a4dc1b442447410ed8514e61de4590a1
Joseph <jposc539@students.bju.edu>
2013-12-16 19:14:38 -0500

    saved latest changes

--- js/collections-create.js
+++ js/collections-create.js
@@ -6,33 +6,7 @@ $(document).ready(function(){
 
     // set our Parse keys
     Parse.initialize("FPOlmLXJUEua1HOYMHHN8TPCvtlouzpj9LSzjMh0", "bh2d38avzROSyB9JzQa68tVtu3VnhZcXsdbTejl9");
-
-    // This is the HTML template for a collection item
-    // The template system is called handlebars and provdide by underscore.js
-    // See here for more info: http://underscorejs.org/#template
-    var collectionTemplate = " \
-    <div class='col-md-3 col-sm-6 collection-item <%= collectionType %>'> \
-        <a href='#' class='thumbnail collection-button existing-collection'> \
-            <h1 class='thumbnail-attribute thumbnail-name'><%= collectionName %></h1> \
-            <h3 class='thumbnail-attribute thumbnail-type'><%= collectionType %></h3> \
-            <h3 class='thumbnail-attribute thumbnail-count'>Items: 0</h3> \
-            <p class='thumbnail-attribute thumbnail-desc'><%= collectionDesc %></p> \
-        </a> \
-    </div> \
-    <div class='arrow'></div>"
-
-    // ----------------- original template -----------------
-    /*var collectionTemplate = " \
-    <div class='col-md-3 col-sm-6 collection-item <%= collectionType %>'> \
-        <a href='#' class='thumbnail collection-button existing-collection'> \
-            <h1 class='collection-name'><%= collectionName %></h1> \
-            <h1 class='thumbnail-attribute thumbnail-name'><%= collectionName %></h1> \
-            <h3 class='item-count'><%= collectionType %></h3> \
-            <p class='edit-link'><%= collectionDesc %></p> \
-        </a> \
-    </div> \
-    <div class='arrow'></div>"*/
-
+    
     /*------------ Load the collections from Parse ------------*/
     var CollectionObject = Parse.Object.extend("Collection");
     var query = new Parse.Query(CollectionObject);
@@ -41,44 +15,65 @@ $(document).ready(function(){
         // loop through each collection in the results (an array)
         for (i=0;i<results.length;++i) {
             var collection = results[i];
-            // Render the HTML using the model data and the handlebars template
-            var collectionHTML = _.template(collectionTemplate, collection.attributes);
-            // Add the rendered collection to the container
-            $("#collection-grid").append(collectionHTML);
+            renderCollectionThumbnail(collection);
         }
     });
 
 });
 
+
+function renderCollectionThumbnail(collectionModel) {
+	var collectionTemplate = " \
+    <div class='col-md-3 col-sm-6'> \
+        <a href='#' class='thumbnail'> \
+            <h1 class='thumbnail-attribute thumbnail-name'><%= collectionName %></h1> \
+            <h3 class='thumbnail-attribute thumbnail-type'><%= collectionType %></h3> \
+            <h3 class='thumbnail-attribute thumbnail-count'>Items: 0</h3> \
+            <p class='thumbnail-attribute thumbnail-desc'><%= collectionDesc %></p> \
+        </a> \
+        <div class='arrow'></div> \
+    </div>"
+
+    // Render the HTML using the model data and the handlebars template
+    var collectionHTML = _.template(collectionTemplate, collectionModel.attributes);
+    // Add the rendered collection to the container
+    $("#collection-grid a.new-collection").parent().before(collectionHTML);
+}
+
 /*------------ Create New Collection ------------*/
 
 $('#btnCreateCollection').click(function() {
-  // create a collection
-  var Collection = Parse.Object.extend("Collection");
-  var collection = new Collection();
 
-  // save data from forms into the new collection
-  var thisCollectionName = document.getElementById('inputCollectionName').value;
-  var thisCollectionType = document.getElementById('inputCollectionType').value;
-  var thisCollectionDesc = document.getElementById('inputCollectionDesc').value;
+    /* create a collection */
+    var Collection = Parse.Object.extend("Collection");
+    var collection = new Collection();
 
-  collection.set("collectionName", thisCollectionName);
-  collection.set("collectionType", thisCollectionType);
-  collection.set("collectionDesc", thisCollectionDesc);
-  collection.save();
+    /* save data from forms into the new collection */
+    var thisCollectionName = document.getElementById('inputCollectionName').value;
+    var thisCollectionType = document.getElementById('inputCollectionType').value;
+    var thisCollectionDesc = document.getElementById('inputCollectionDesc').value;
 
-  /* resets form fields to blank */
-  $('#inputCollectionName').val(""); 
-  $('#inputCollectionType').val("");
-  $('#inputCollectionDesc').val("");
+    
 
-  alert("You have successfully created\na new collection named " + thisCollectionName);
+    collection.set("collectionName", thisCollectionName);
+    collection.set("collectionType", thisCollectionType);
+    collection.set("collectionDesc", thisCollectionDesc);
+    collection.save();
 
-  /* adds collection icon representing new collection */
-  /* $('.new-collection').before("<a class='collection-button existing-collection' href='#'><p class='collection-name'>I do not exist yet!</p><p class='item-count'>Items: 0</p><p class='edit-link'>Edit</p><div class='arrow'></div></a>");
-  $('a.existing-collection div.arrow').hide(); */
+    /* resets form fields to blank */
+    $('#inputCollectionName').val(""); 
+    $('#inputCollectionType').val("");
+    $('#inputCollectionDesc').val("");
 
-  return false;
+    renderCollectionThumbnail(collection);
+
+    //alert("You have successfully created\na new collection named " + thisCollectionName);
+
+    /* adds collection icon representing new collection */
+    /* $('.new-collection').before("<a class='collection-button existing-collection' href='#'><p class='collection-name'>I do not exist yet!</p><p class='item-count'>Items: 0</p><p class='edit-link'>Edit</p><div class='arrow'></div></a>");
+    $('a.existing-collection div.arrow').hide(); */
+
+    return false;
 });
 
     /*
